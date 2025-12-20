0ffcca7c54c88243046a724337c08798a5945395
leydel <me@itsf.in>
2025-12-20 23:43:05 +1000

    Add template metadata to vertices and edges

--- Numberlink.ZigZag.Core/Level.fs
+++ Numberlink.ZigZag.Core/Level.fs
@@ -3,6 +3,7 @@ namespace Numberlink.ZigZag.Core
 open System
 
 type Level = {
+    Id: Guid
     Template: Template
     Links: Guid list list
 }
--- Numberlink.ZigZag.Core/Template.fs
+++ Numberlink.ZigZag.Core/Template.fs
@@ -1,49 +1,83 @@
 namespace Numberlink.ZigZag.Core
 
+open FsToolkit.ErrorHandling
 open Numberlink.ZigZag.Core.Lib
-open System
 
-type Coordinate =
-    | Orthogonal of int * int
+type TemplateVertexPosition =
+    | Orthogonal of x: int * y: int
 
-type TemplateVertex =
-    | Unknown
+type TemplateVertexType =
+    | Unobserved
     | Bridge
 
+type TemplateVertex = {
+    Type: TemplateVertexType
+    Position: TemplateVertexPosition
+}
+
+type TemplateEdgeType =
+    | Path
+    | Warp
+
+type TemplateEdge = {
+    Type: TemplateEdgeType
+}
+
 type Template = {
-    Id: Guid
-    Graph: Graph<TemplateVertex, unit>
-    Layout: Map<Guid, Coordinate>
+    Graph: Graph<TemplateVertex, TemplateEdge>
 }
 
 module Template =
-    /// Create an empty template graph.
-    let create id = {
-        Id = id
-        Graph = Graph.empty
-        Layout = Map.empty
-    }
+    /// Create an empty template.
+    let empty =
+        { Graph = Graph.empty }
 
-    /// Add an uncomputed vertex to the template graph to generate levels off.
-    let addVertex vertexId coordinate (template: Template) =
-        let graph = Graph.addVertex vertexId Unknown template.Graph
-        let layout = Map.add vertexId coordinate template.Layout
+    /// Add an unobserved vertex to the template graph to generate levels off.
+    let addUnobserved vertexId position (template: Template) =
+        let vertex = { Type = Unobserved; Position = position }
+        let graph = Graph.addVertex vertexId vertex template.Graph
 
-        { template with Graph = graph; Layout = layout }
+        { template with Graph = graph }
 
     /// Add a bridge vertex to the template graph.
-    let addBridge vertexId coordinate (template: Template) =
-        let graph = Graph.addVertex vertexId Bridge template.Graph
-        let layout = Map.add vertexId coordinate template.Layout
+    let addBridge vertexId position (template: Template) =
+        let vertex = { Type = Bridge; Position = position }
+        let graph = Graph.addVertex vertexId vertex template.Graph
         
-        { template with Graph = graph; Layout = layout }
+        { template with Graph = graph }
+        
+    /// Remove any type of vertex from the template graph.
+    let removeVertex vertexId (template: Template) =
+        let graph = Graph.removeVertex vertexId template.Graph
 
-    /// Add an edge between two vertices in the template graph.
-    let addEdge fromVertexId toVertexId edgeId (template: Template) =
-        let graph = Graph.addEdge fromVertexId toVertexId edgeId () template.Graph
+        { template with Graph = graph }
+
+    /// Add a possible path edge between two vertices in the template graph.
+    let addPath fromVertexId toVertexId edgeId (template: Template) =
+        let edge = { Type = Path }
+        let graph = Graph.addEdge fromVertexId toVertexId edgeId edge template.Graph
         
         { template with Graph = graph }
 
-    /// Validate the template to ensure basic requirements to generate levels from it are met.
-    let validate (template: Template) =
-        raise<bool> (System.NotImplementedException()) // TODO: Implement
+    /// Add a warp edge between two vertices in the template graph.
+    let addWarp fromVertexId toVertexId edgeId (template: Template) =
+        let edge = { Type = Warp }
+        let graph = Graph.addEdge fromVertexId toVertexId edgeId edge template.Graph
+        
+        { template with Graph = graph }
+
+    /// Remove an edge from the template graph.
+    let removeEdge edgeId (template: Template) =
+        let graph = Graph.removeEdge edgeId template.Graph
+
+        { template with Graph = graph }
+
+    /// Validate the template for pre-generation requirements.
+    let validate (template: Template) = result {
+        // TODO: Ensure vertices have the same vertex layout type
+        // TODO: Ensure edges connect existing vertices
+        // TODO: Ensure bridges have an even number of connections
+        // TODO: Ensure warps aren't stacked
+
+        do! Error "not implemented"
+    }
